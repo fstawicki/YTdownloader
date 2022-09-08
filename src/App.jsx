ff555494444c2a3ec38fe0bc058c809826f19ad8
fstawicki <stawickifilip1@gmail.com>
2022-09-09 00:06:55 +0200

    done logic and responsiveness. TODO: change background and UI styling accordingly. Add yt icon in h1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,20 @@ import './App.css';
 function App() {
 
   const [input, setInput] = useState('');
+  const [show, setShow] = useState(true);
+
+  const [linkMP3, setLinkMP3] = useState('');
+  const [link144p, setLink144p] = useState('');
+  const [link240p, setLink240p] = useState('');
+  const [link360p, setLink360p] = useState('');
+  const [link480p, setLink480p] = useState('');
+  const [link720p, setLink720p] = useState('');
+  const [link1080p, setLink1080p] = useState('');
+
+  const [videoName, setVideoName] = useState('');
+  const [link, setLink] = useState('');
+  const [videoImage, setVideoImage] = useState('');
+  const [videoDuration, setVideoDuration] = useState('');
 
   const getInput = (e) => {
     setInput(e.target.value);
@@ -26,21 +40,96 @@ function App() {
     };
     
     axios.request(options).then(function (response) {
-      console.log(response.data);
+      const data = response.data;
+      console.log(data);
+      setVideoName(data.title);
+      setVideoDuration(data.duration);
+      setVideoImage(data.thumbnail);
+
+      data.medias.forEach(element => {
+        if(element.extension === 'mp3' && element.quality === '128kbps'){
+          setLinkMP3(element.url);
+        }
+        if(element.extension === 'mp4' && element.quality === '144p'){
+          setLink144p(element.url);
+        }
+        if(element.extension === 'mp4' && element.quality === '240p'){
+          setLink240p(element.url);
+        }
+        if(element.extension === 'mp4' && element.quality === '360p'){
+          setLink360p(element.url);
+        }
+        if(element.extension === 'mp4' && element.quality === '480p'){
+          setLink480p(element.url);
+        }
+        if(element.extension === 'mp4' && element.quality === '720p'){
+          setLink720p(element.url);
+        }
+        if(element.extension === 'mp4' && element.quality === '1080p'){
+          setLink1080p(element.url);
+        }
+      });
+
+
     }).catch(function (error) {
       console.error(error);
+      alert('Something went wrong');
     });
+  }
+
+  const getQuality = (e) => {
+    const quality = e.target.value;
+    switch(quality){
+      case '144p':
+        setLink(link144p)
+        break;
+      case '240p':
+        setLink(link240p)
+        break;
+      case '360p':
+        setLink(link360p)
+        break;
+      case '480p':
+        setLink(link480p)
+        break;
+      case '720p':
+        setLink(link720p)
+        break;
+      case '1080p':
+        setLink(link1080p)
+        break;
+      default:
+    }
   }
 
   return (
     <div className="App">
       <div className="container">
         <h1>YT Downloader</h1>
-        <p>Enter Youtube link below and click convert</p>
+        <p className='instruction'>Enter Youtube link below and click convert</p>
         <div className="searchbar">
           <input type="text" value={input} onChange={getInput} />
-          <button onClick={convertLink}>Convert</button>
+          <button className='convertBtn' onClick={convertLink}>Convert</button>
         </div>
+        {show && (
+        <>
+        <p className='info'>{videoName}</p>
+        <p className='info'>Duration: {videoDuration}</p>
+        <img className='image' src={videoImage} alt="yt thumbnail" />
+        <div className="downloadContainer">
+          <a href={linkMP3} className='downloadBtn'>Download MP3</a>
+          <select name="quality" defaultValue="144p" onChange={getQuality}>
+            <option value="144p">144p</option>
+            <option value="240p">240p</option>
+            <option value="360p">360p</option>
+            <option value="480p">480p</option>
+            <option value="720p60">720p60</option>
+            <option value="1080p60">1080p60</option>
+          </select>
+          <a href={link} className='downloadBtn'>Download MP4</a>
+        </div>
+        </>
+        )}
       </div>
     </div>
   );
